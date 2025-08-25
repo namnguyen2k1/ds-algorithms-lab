357e9d7845dab30cf5d7e591bf334866678b6ed7
namnguyen2k1 <nanam133hg@gmail.com>
2025-08-25 23:29:32 +0700

    feat: selection sort algorithm

---
+++ src/sort-algorithms/selection-sort.ts
@@ -0,0 +1,19 @@
+import { swap } from '../utils/swap';
+
+export function selectionSort(arr: number[]) {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      swap(arr, i, minIndex);
+    }
+  }
+
+  return arr;
+}
