1ab2deda903ebb4aa203631a51a85cc23882e174
namnguyen2k1 <nanam133hg@gmail.com>
2025-08-25 23:31:47 +0700

    feat: tim sort algorithm

---
+++ src/sort-algorithms/tim-sort.ts
@@ -0,0 +1,49 @@
+function insertionSort(arr: number[], left: number, right: number) {
+  for (let i = left + 1; i <= right; i++) {
+    const key = arr[i];
+    let j = i - 1;
+    while (j >= left && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+}
+
+function merge(arr: number[], l: number, m: number, r: number) {
+  const leftArr = arr.slice(l, m + 1);
+  const rightArr = arr.slice(m + 1, r + 1);
+  let i = 0,
+    j = 0,
+    k = l;
+  while (i < leftArr.length && j < rightArr.length) {
+    if (leftArr[i] <= rightArr[j]) {
+      arr[k++] = leftArr[i++];
+    } else {
+      arr[k++] = rightArr[j++];
+    }
+  }
+  while (i < leftArr.length) arr[k++] = leftArr[i++];
+  while (j < rightArr.length) arr[k++] = rightArr[j++];
+}
+
+export function timSort(arr: number[]) {
+  const n = arr.length;
+  const RUN = 32;
+
+  for (let i = 0; i < n; i += RUN) {
+    insertionSort(arr, i, Math.min(i + RUN - 1, n - 1));
+  }
+
+  for (let size = RUN; size < n; size *= 2) {
+    for (let left = 0; left < n; left += 2 * size) {
+      const mid = Math.min(left + size - 1, n - 1);
+      const right = Math.min(left + 2 * size - 1, n - 1);
+      if (mid < right) {
+        merge(arr, left, mid, right);
+      }
+    }
+  }
+
+  return arr;
+}
